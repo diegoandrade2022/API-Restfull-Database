ea8995d5f62ca78304dd9c307c2df8e494a2fde5
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-06-15 13:29:02 -0300

    Excluindo função strigFiltro, após encontrar nova resolução

--- src/uteis/validacoes.js
+++ src/uteis/validacoes.js
@@ -44,52 +44,12 @@ const transacaoExiste = async (id, usuario_id) => {
   }
   return transacao.rows[0]
 }
-const possuiTransacoes = async usuario_id => {
-  let transacao = await query(
-    'SELECT * FROM transacoes WHERE usuario_id = $1',
-    [usuario_id]
-  )
 
-  if (transacaoExiste.rowCount <= 0) {
-    return false
-  }
-  return transacao.rows[0]
-}
-
-const stringFiltro = arrFiltro => {
-  if (arrFiltro) {
-    let arrayFormatada = []
-    arrFiltro.forEach(item => {
-      let palavraSeparada = []
-      if (item.includes(' ')) {
-        let a = item.split(' ')
-        for (itens of a) {
-          if (itens.length <= 3) {
-            palavraSeparada.push(itens.toLowerCase())
-          } else {
-            palavraSeparada.push(itens[0].toUpperCase() + itens.slice(1).toLowerCase())
-          }
-        }
-        arrayFormatada.push(palavraSeparada.join(' '))
-      } else {
-        let palavra = item[0].toUpperCase() + item.slice(1).toLowerCase()
-        arrayFormatada.push(palavra)
-      }
-    })
-
-    let string = arrayFormatada.join("','")
-    let filtro = `AND c.descricao in ('${string}')`
-    return filtro
-  }
-  return ''
-}
 module.exports = {
   usuarioComEmail,
   retornaHash,
   existeCategoria,
-  transacaoExiste,
-  possuiTransacoes,
-  stringFiltro
+  transacaoExiste
 }
 
 
